fe0931b983fca78d715cb60066bf6c83fc2af8bd
tharindarodrigo <tharindarodrigo@gmail.com>
2016-06-06 18:31:10 +0530

    worked on the form section of formSampleOne.ejs

--- SYM/src/main.jsx
+++
@@ -1,5 +0,0 @@
-var react = require('react');
-var ReactDom = require('react-dom');
-var Text = require('./components/Text.jsx');
-
-ReactDom.render(<Text/>, document.getElementById('ingredients'));
