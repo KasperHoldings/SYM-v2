1c16f7b89a4334c7967eb970f606e8def18bfe6e
Lightway <lightway29@gmail.com>
2016-06-09 13:17:56 +0530

    Select Templete update

--- SYM/app.js
+++ SYM/app.js
@@ -5,7 +5,10 @@ var app = express();
 
 var port = process.env.PORT || 5000;
 
-var connection = require('./src/config/dbconfig');
+var connection  = require('express-myconnection');
+var mysql = require('mysql');
+
+
 
 
 var nav = [{
@@ -18,40 +21,39 @@ var nav = [{
     Link: '/formSampleOne',
     Text: 'Sample Form'
 }
-//    ,{
-//    Link: '/sampleUsers',
-//    Text:'Sample User Form'
-//}
+    ,{
+    Link: '/list',
+    Text:'Sample Select Template'
+}
 ];
-
-connection.connect();
 
 var insuranceRouter = require('./src/routes/insuranceRouter')(nav);
 var sampleForm = require('./src/routes/sampleFormRouter')(nav);
-
-//Database Connection Sample
-{
-
-
-    connection.query('SELECT * From sampleTable', function (err, rows, fields) {
-        if (err) throw err;
-
-        console.log('The solution is: ', rows[0]);
-
-    });
-
-    //connection.end();
-
-}
-
+var userRouter = require('./src/routes/userRouter');
 
 app.use(express.static('public'));
 app.set('views','./src/views');
 app.set('view engine','ejs');
 
+
+app.use(
+
+    connection(mysql,{
+
+        host: 'localhost',
+        user: 'root',
+        password : '123',
+        port : 3306, //port mysql
+        database:'dbsym'
+
+    },'pool') //or single
+
+);
+
 app.use('/',insuranceRouter);
 app.use('/',sampleForm);
 
+app.get('/list', userRouter.list);
 app.get('/',function(req,res){
     res.render('index',{
         title:'Home',
