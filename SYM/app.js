38ab50cf012ea20b3585d34a308bb944d929fa2e
tharindarodrigo <tharindarodrigo@gmail.com>
2016-06-09 13:48:13 +0530

    userRegistration.ejs Added

--- SYM/app.js
+++ SYM/app.js
@@ -1,30 +1,27 @@
-
 var express = require('express');
 
 var app = express();
 
 var port = process.env.PORT || 5000;
 
-var connection  = require('express-myconnection');
+var connection = require('express-myconnection');
 var mysql = require('mysql');
-
-
 
 
 var nav = [{
     Link: '/Bike',
     Text: 'Bike'
-},{
+}, {
     Link: '/Three',
-    Text:'Three'
+    Text: 'Three'
 }, {
     Link: '/formSampleOne',
     Text: 'Sample Form'
 }
-    ,{
-    Link: '/list',
-    Text:'Sample Select Template'
-}
+    , {
+        Link: '/list',
+        Text: 'Sample Select Template'
+    }
 ];
 
 var insuranceRouter = require('./src/routes/insuranceRouter')(nav);
@@ -32,36 +29,42 @@ var sampleForm = require('./src/routes/sampleFormRouter')(nav);
 var userRouter = require('./src/routes/userRouter');
 
 app.use(express.static('public'));
-app.set('views','./src/views');
-app.set('view engine','ejs');
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 
 app.use(
-
-    connection(mysql,{
+    connection(mysql, {
 
         host: 'localhost',
         user: 'root',
-        password : '123',
-        port : 3306, //port mysql
-        database:'dbsym'
+        password: '1234',
+        port: 3306, //port mysql
+        database: 'dbsym'
 
-    },'pool') //or single
+    }, 'pool') //or single
 
 );
 
-app.use('/',insuranceRouter);
-app.use('/',sampleForm);
+app.use('/', insuranceRouter);
+app.use('/', sampleForm);
 
 app.get('/list', userRouter.list);
-app.get('/',function(req,res){
-    res.render('index',{
-        title:'Home',
-        nav : nav
+app.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Home',
+        nav: nav
     });
 
 });
 
-app.listen(port,function(err){
-    console.log('Running server on port '+port);
+app.get('/userRegistration', function (req, res) {
+    res.render('userRegistration', {
+        title: 'Home',
+        nav: nav
+    });
+});
+
+app.listen(port, function (err) {
+    console.log('Running server on port ' + port);
 });
